3a381424e903294651b4e858107912cabc9d8b06
nox-interior <mariam.arroyo@gmail.com>
2025-02-17 17:29:36 +0100

    Exercises directory + ex_01, ex_02

---
+++ exercises/ex_01.py
@@ -0,0 +1,17 @@
+# We define a variable x containing a string
+x = "The value of (a + b) * c is"
+
+# We can make multiple assignments in a single line
+a, b, c = 1, 2, 3
+
+# Let's define the operation we want to perform
+d = (a + b) * c
+
+# Let's define a boolean variable
+printing = True
+
+# if printing is true, then use print()
+if printing:
+    print(x, d)
+
+# Expected output: The value of (a + b) * c is 9
---
+++ exercises/ex_02.py
@@ -0,0 +1,5 @@
+def function(a, b, c):
+    return a+b+c
+d = function(10, 23, 3)
+s = "Result: "
+print (s,    d)

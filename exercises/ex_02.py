153137189df79db8e8e947dfc270d299f9e60fce
nox-interior <mariam.arroyo@gmail.com>
2025-02-20 11:07:57 +0100

    new exercises and types theory .md

--- exercises/ex_02.py
+++ exercises/ex_02.py
@@ -2,4 +2,4 @@ def function(a, b, c):
     return a+b+c
 d = function(10, 23, 3)
 s = "Result: "
-print (s,    d)
+print (s, d)
---
+++ exercises/ex_03.py
@@ -0,0 +1,4 @@
+# Apparently, to be researched, strings can be contained between simple 
+# and double QUOTES
+print('Hola Munda!')
+print("Hola Mundo!")
